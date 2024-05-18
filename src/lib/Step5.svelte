8f7abfe0cb07db799d302e91b69a66a16c1c995a
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-05-18 23:14:08 +0700

    Create a new step5 svelte

---
+++ src/lib/Step5.svelte
@@ -0,0 +1,27 @@
+<style>
+   
+    .header .title {
+    color: var(--Marine-blue);
+    }
+
+    .header .exp {
+    color: var(--Cool-gray);
+    }
+    .step4-container{
+        margin-left: 30px;
+    }
+ 
+</style>
+
+<div class="step4-container">
+    <img src="/icon-thank-you.svg" alt="" />
+        <div class="header">
+            <h1 class="title">Thank you!</h1>
+            <p class="exp">
+              Thanks for confirming your subscription! We hope you have fun
+              using our platform. If you ever need support, please feel free to
+              email us at support@loremgaming.com.
+            </p>
+        </div>
+    <button class="next-stp"></button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-    import Step4 from "$lib/Step4.svelte";
+    import Step5 from "$lib/Step5.svelte";
 </script>
 
 <style>
 
 </style>
 
-<Step4/>
+<Step5/>
 
