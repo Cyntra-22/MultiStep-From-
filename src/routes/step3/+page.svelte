3d0706c9b1858c452ef85632529cd83dd9017fb6
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-05-28 00:36:05 +0700

    Remove the unnecessary codes

--- src/routes/step3/+page.svelte
+++ src/routes/step3/+page.svelte
@@ -19,27 +19,3 @@
 <Step3 onSubmit={handleNextStep} onGoBack={handleGoBack} />
 
 
-
-<!--
-
-<script lang="ts">
-  import Step3 from "$lib/Step3.svelte";
-  import { goto } from '$app/navigation';
-  import { writable } from 'svelte/store';
-
-  export const selectedAddOns = writable([]);
-
-  function handleNextStep(event: Event, addOns: any) {
-    event.preventDefault();
-    selectedAddOns.set(addOns);
-    goto('/step4');
-  }
-
-  function handleGoBack() {
-    goto('/step2');
-  }
-</script>
-
-<Step3 onSubmit={handleNextStep} onGoBack={handleGoBack} />
-
--->
